b4e4c813ad5e714d64e3e754400ddebef77ffbbd
gemperline <agemperline@csu.fullerton.edu>
2019-03-05 22:34:02 -0800

    Output file complete

--- pay.cpp
+++ pay.cpp
@@ -1,70 +1,71 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
 #include "person.cpp"
 
-void readData(Person P[], string fileName, const int SIZE)
+int readData(Person P[], string fileName, const int SIZE, int &numPeople)
 {
   string fName;
   string lName;
   float  hours;
   float  pay;
-  int numPersons = 0;
 
   ifstream inFile;
   inFile.open("input.txt");
 
   if (inFile.is_open())
   {
-    cout << "File opened successfully ..." << endl;
+    cout << "File opened successfully ...\n" << endl;
 
-    while(inFile >> fName && numPersons < SIZE)
+    while(inFile >> fName && numPeople < SIZE)
     {
       inFile >> lName >> hours >> pay;
 
-      P[numPersons].setFirstName(fName);
-      P[numPersons].setLastName(lName);
-      P[numPersons].setHoursWorked(hours);
-      P[numPersons].setPayRate(pay);
+      P[numPeople].setFirstName(fName);
+      P[numPeople].setLastName(lName);
+      P[numPeople].setHoursWorked(hours);
+      P[numPeople].setPayRate(pay);
 
-      cout << P[numPersons].getFirstName() << endl;
-      numPersons++;
+      numPeople++;
     }
-    cout << "Number of people: " << numPersons << endl;
-
     inFile.close();
   }
   else
     cout << "ERROR: could not open file ..." << endl;
+
+    return numPeople;
 }
 
 
-void writeData(Person P[], string fileName, const int SIZE)
+void writeData(Person P[], string fileName, int numPeople)
 {
   ofstream outFile;
-  outFile.open(filename);
+  outFile.open(fileName);
 
-  for(int i = 0; i < SIZE; i++)
+  for(int i = 0; i < numPeople; i++)
   {
-    outFile << P[i].fullName << P[i].totalPay << endl;
+    outFile << P[i].fullName() << " " << fixed << setprecision(2) << P[i].totalPay() << endl;
+    cout << P[i].fullName() << endl;
   }
+  outFile.close();
 }
 
 
 int main()
 {
+  int numPeople = 0;
   const int SIZE = 20;
   // create an array of 20 empty people
   Person employees[SIZE];
 
   // read the data from file into the array
-  readData(employees, "input.txt", SIZE);
-
+  readData(employees, "input.txt", SIZE, numPeople);
   // write data to an output file
-  writeData(employees, "output.txt", SIZE);
+  writeData(employees, "output.txt", numPeople);
 
   return 0;
 }
